a86143b9dad8c25b6452fd36b7e05befc4b4e9dc
Alonso Anchante Moreno <alonso.amoreno@live.com>
2023-07-15 10:29:09 -0500

    Validar Formulario

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref, onMounted, reactive } from "vue";
+import Alerta from "./components/Alerta.vue";
 const monedas = ref([
   { codigo: "USD", texto: "Dolar de Estados Unidos" },
   { codigo: "MXN", texto: "Peso Mexicano" },
@@ -13,6 +14,7 @@ const cotizar = reactive({
   moneda: "",
   cripto: "",
 });
+const error = ref("");
 
 onMounted(() => {
   const url =
@@ -21,13 +23,24 @@ onMounted(() => {
     .then((response) => response.json())
     .then(({ Data }) => (criptomonedas.value = Data));
 });
+
+const cotizarCripto = () => {
+  // Validar que cotizar esté lleno
+  if (Object.values(cotizar).includes("")) {
+    error.value = "Todos los campos son obligatorios";
+    return;
+  }
+  error.value = "";
+  console.log("cotizando...");
+};
 </script>
 
 <template>
   <div class="contenedor">
     <h1 class="titulo">Cotizador de <span>Criptomonedas</span></h1>
     <div class="contenido">
-      <form class="formulario">
+      <Alerta v-if="error">{{ error }}</Alerta>
+      <form @submit.prevent="cotizarCripto" class="formulario">
         <div class="campo">
           <label for="moneda">Moneda:</label>
           <select id="moneda" v-model="cotizar.moneda">
